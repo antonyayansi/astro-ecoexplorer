ab0ce6e4a342492b83f20aa905a7f84e253894fb
Antony Ayansi <toileok1@gmail.com>
2024-05-26 18:30:22 -0500

    26/05

---
+++ src/utils/data.ts
@@ -0,0 +1,40 @@
+const services = [
+    {
+        title: "Modelos de Predicción Ambiental",
+        description: "Utilizamos algoritmos avanzados para predecir tendencias ambientales y proporcionar información predictiva sobre cambios en la temperatura, la radiación y otros factores ambientales clave.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "API para Integración con Otras Aplicaciones",
+        description: "Ofrecemos una API robusta que permite la integración sin problemas de los datos de EcoExplorer con otras aplicaciones y plataformas, ampliando su utilidad y alcance.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Análisis Avanzado de Datos",
+        description: "Nuestro equipo de expertos realiza análisis detallados de los datos recopilados por EcoExplorer, proporcionando información significativa y acciones recomendadas para optimizar la toma de decisiones.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Monitoreo en Tiempo Real",
+        description: "EcoExplorer ofrece monitoreo continuo en tiempo real de los parámetros ambientales, permitiendo una respuesta rápida ante cambios significativos y emergencias ambientales.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Informes Personalizados",
+        description: "Generamos informes personalizados según las necesidades específicas de nuestros clientes, proporcionando insights relevantes y acciones concretas para mejorar la gestión ambiental.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Soporte Técnico Especializado",
+        description: "Nuestro equipo de soporte técnico altamente capacitado está disponible para brindar asistencia y resolver cualquier consulta relacionada con EcoExplorer y su funcionamiento.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+];
+
+export { services };
